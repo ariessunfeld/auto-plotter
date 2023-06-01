6adad96ca0ff2d166c155ed946407ecf5eccb8ef
ariessunfeld <100384173+ariessunfeld@users.noreply.github.com>
2023-06-01 14:45:52 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,49 @@
+# What is Auto Plotter?
+
+Auto Plotter is designed to make data visualization as easy as possible. You can give it CSV, TSV, XLSX, JSON, or other data products, simply by copying the file(s) into the auto-plotter folder, and then describe what kind of plot(s) you want made from your data. It does the rest. 
+
+## Details
+
+Auto Plotter uses GPT 4 to write python code to generate the kind of plot(s) you described, using the provided file(s) as directed. It then uses GPT 3.5 to add error handling to the code it wrote and to analyze it for safety. After this process, you have the option to view and/or run the code it produced to generate the kinds of visualizations you requested.
+
+# Usage
+
+To launch Auto-Plotter, simply run `python3 auto-plotter.py` in the terminal or command prompt. Assuming the installation and setup process went smoothly, a GUI (graphical user interface) should open. In the text window at the very bottom of the GUI, you can type your request to Auto Plotter, and when you're ready, click send. If you reference files on your computer in your request, make sure that they are The status of the process will be displayed in the terminal or command prompt while Auto Plotter thinks about your message and then writes and validates code in response to your message.
+
+## Example Usage
+
+Some example requests are listed below. For each of these requests, Auto Plotter will assume that the files mentioned by name in the request are present in the auto-plotter directory, and that any column names / keys listed in the request are present in the corresponding files.
+* "I have a file called data.csv with columns for SiO2, FeOT, and MnO, and a column called Category. I want a scatterplot of SiO2 on the y axis and FeOT on the x axis with a continuous MnO heatmap, just for the data in category A."
+* I have a file called data.csv and another file called categories.csv. The data.csv file has a Target column and chemistry columns. The categories.csv has a Target column and a Category column. I want a single heatmap showing the pearson's r correlation coefficients between MnO and the following oxides [SiO2, FeOT, MgO] across all the categories. The categories should appear on the horizontal axis of the heatmap, and the three oxides along the vertical axis.
+
+# Setup
+
+Auto Plotter requires that you have Python and Pip installed on your computer, and that you have an OpenAI API key. The API key is free to obtain, but a payment method is required, as you will be charged a tiny amount (one or two cents) for typical usage.
+
+## Python Installation
+
+If you don't have it already, download and install python from https://www.python.org/downloads/. There should be a yellow Download button for the latest version. When installing, make sure to add python to PATH if prompted.
+
+## Pip Installation
+
+Open the terminal or command prompt, execute `curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py`, then execute `python3 get-pip.py` and follow the installation prompts. Pip should now be installed on your system.
+
+## OpenAI API key
+
+If you don't have an OpenAI API key already, go to platform.openai.com/account/api-keys. If you don't have an OpenAI account yet, you will have to sign up with your email. Then follow the instructions to add a payment method. You can set a usage limit so that no more than $X is charged to your card each month. For typical usage of Auto Plotter, a usage limit of $5 should be more than enough.
+
+## Cloning repository
+
+If you have Git installed on your computer, copy the cloning link for this repository, then open the terminal or command prompt and run `git clone link-goes-here`. If you prefer to simply download the code as a zip file, you can do that, too.
+
+## Environment setup
+
+Open the Finder or File Browser and unzip the code directory (folder) (if you didn't clone the repository with Git). Open a new terminal or command prompt and navigate into the auto-plotter directory. (To navigate in the terminal or command promot, use `cd`.)Then execute `ls` on Mac, or `dir` on Windows to ensure that you have these files: `requirements.txt`, `template.env`, and `auto-plotter.py`. Open `template.env` and replace the text `your-api-key-goes-here` with your full API key from OpenAI. Then rename `template.env` to `.env`. You can do this in the terminal or command prompt by executing the following command: `mv template.env .env`. 
+
+## Installing dependencies
+
+### Creating a virtual environment
+
+It's recommended to install dependencies in a virtual environment to avoid conflicts with other packages you might have installed. To do this, navigate into the auto-plotter directory on the terminal or command prompt, then run `python3 -m venv venv`. This will create a folder called `venv`. To activate the new virtual environment you just created, run `source venv/bin/activate` (on macOS) or `.\venv\Scripts\activate` (on Windows). 
+
+Now run `pip install -r requirements.txt`. You will need to be connected to the internet for this step to work. This will install the 3rd-party python libraries necessary for auto-plotter to work, but instead of installing them in your base environment, they will be installed in the virtual environment.
---
+++ auto-plotter.py
@@ -0,0 +1,178 @@
+import os
+import openai
+import tkinter as tk
+from tkinter import ttk
+import atexit
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+DATA_VIZ_SYSTEM_DESCRIPTION = "You are a helpful data visualization assistant. I am going to describe what kind of visualizations I want, and you will respond with exactly the python code necessary to generate these visualizations, and no other output. You can assume I have matplotlib, numpy, pandas, plotly, and seaborn installed. All plots should be made with high resolution, labeled clearly, and saved to disk. Your output will be put directly into a python file to be executed, so do not include any unecessary commentary."
+ERROR_HANDLING_SYSTEM_DESCRIPTION = 'You specialize in adding error handling to python code. The code you will receive is going to be called as a string inside an `exec` function in a larger python script. Your job is to add error handling to each file-read or file-write line. Error handling should raise Exceptions with informative messages if files cannot be read, if columns do not exist in dataframes, or anything similar. For example, if a FileNotFoundError is going to be raised, then raise a FileNotFoundError with an informative message, including the name of the file not found. If files are being written, error handling should ensure that no file with the same name already exists, and if it does, should prompt the user for confirmation before overwriting. You should add this error handling to the python code you receive, and respond with the updated code, but nothing else: no commentary.'
+CODE_SAFETY_SYSTEM_DESCRIPTION = 'You are a code safety analyst. You analyze python code and ensure that nothing harmful or unusual will take place if the code is executed. You expect the code to be a data visualization script, usually a combination of reading spreadsheets and creating and saving plots. If you deem the code safe to run, you will respond with "All clear" and nothing else. Otherwise, if you see anything suspicious or concerning, you will respond with "Dangerous. Do not proceed" and nothing else.'
+
+def send_message():
+    message = user_input.get()
+    conversation.insert(tk.END, "You: " + message + "\n")
+    user_input.delete(0, tk.END)
+
+    print('Thinking...')
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "system", 
+                "content": DATA_VIZ_SYSTEM_DESCRIPTION
+            },
+            *previous_messages,
+            {"role": "user", "content": message},
+        ]
+    )
+    
+    assistant_response = response.choices[0].message["content"]
+    assistant_lines = assistant_response.split('\n')
+
+    if assistant_lines[0].strip() == "```python" and assistant_lines[-2].strip() == "```":
+        assistant_response = '\n'.join(assistant_lines[1:-2])
+
+    with open('output.py', 'w') as f:
+        f.write(assistant_response)
+
+    print('Done thinking. Preliminary code written to output.py.')
+
+    conversation.insert(tk.END, "Data Viz Assistant:\n", "bold")
+    conversation.insert(tk.END, assistant_response + "\n")
+
+    root.update_idletasks()
+
+    print('Adding error handling to code...')
+
+    error_handling_response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {
+                'role': 'system',
+                'content': ERROR_HANDLING_SYSTEM_DESCRIPTION
+            },
+            {
+                'role':'user',
+                'content':assistant_response
+            }
+        ]
+    )
+
+    error_handling_response = error_handling_response.choices[0].message["content"]
+    error_handling_lines = error_handling_response.split('\n')
+
+    if error_handling_lines[0].strip() == "```python" and error_handling_lines[-2].strip() == "```":
+        error_handling_response = '\n'.join(error_handling_lines[1:-2])
+
+    with open('error-handling-output.py', 'w') as f:
+        f.write(error_handling_response)
+
+    print('Done adding error handling. Finished code written to error-handling-output.py.')
+
+    conversation.insert(tk.END, "\n\nError Handling Assistant:\n", "bold")
+    conversation.insert(tk.END, error_handling_response + "\n")
+
+    root.update_idletasks()
+
+    print('Analyzing code safety...')
+
+    safety_response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {
+                'role':'system',
+                'content': CODE_SAFETY_SYSTEM_DESCRIPTION
+            },
+            {
+                'role':'user',
+                'content': error_handling_response
+            }
+        ]
+    )
+
+    safety_response = safety_response.choices[0].message["content"]
+
+    conversation.insert(tk.END, "\n\nCode Safety Assistant: ", "bold")
+    conversation.insert(tk.END, safety_response + "\n")
+
+    if safety_response.startswith('All clear'):
+        print('Code deemed safe.')
+    if safety_response.startswith('Dangerous'):
+        print('WARNING: Code deemed dangerous. Removing python files from disk.')
+        os.remove('output.py')
+        os.remove('error-handling-output.py')
+
+    previous_messages.extend([
+        {"role": "user", "content": message},
+        {"role": "assistant", "content": assistant_response},
+    ])
+
+def view_output():
+    with open('output.py', 'r') as f:
+        content = f.read()
+    conversation.insert(tk.END, "Output.py content:\n", "bold")
+    conversation.insert(tk.END, content + "\n")
+
+def execute_output():
+    #output = subprocess.check_output(['python3', 'error-handling-output.py']).decode('utf-8')
+    #conversation.insert(tk.END, "Execution result:\n", "bold")
+    #conversation.insert(tk.END, output + "\n")
+    with open("error-handling-output.py", "r") as f:
+        script_code = f.read()
+    try:
+        exec(script_code)
+    except Exception as e:
+        print('Halting process due to error: ', e)
+        conversation.insert(tk.END, "Halting process due to error: \n", "bold")
+        conversation.insert(tk.END, str(e) + "\n")
+
+def save_chat_history():
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"chat_history_{timestamp}.txt"
+    with open(filename, 'w') as f:
+        f.write(conversation.get("1.0", tk.END))
+
+def exit_program():
+    save_chat_history()
+    root.destroy()
+
+if __name__ == '__main__':
+
+    atexit.register(save_chat_history)
+
+    print('Booting up...')
+
+    root = tk.Tk()
+    root.title("Auto Plotter")
+
+    frame = ttk.Frame(root, padding="10")
+    frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+    conversation = tk.Text(frame, wrap=tk.WORD, width=75, height=20, font=("TkDefaultFont", 12))
+    conversation.tag_configure("bold", font=("TkDefaultFont", 12, "bold"))
+    conversation.grid(row=0, column=0, columnspan=4, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+    user_input = ttk.Entry(frame, width=70, font=("TkDefaultFont", 12))
+    user_input.grid(row=1, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+    send_button = ttk.Button(frame, text="Send", command=send_message)
+    send_button.grid(row=1, column=1, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+    view_button = ttk.Button(frame, text="View output.py", command=view_output)
+    view_button.grid(row=1, column=2, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+    execute_button = ttk.Button(frame, text="Execute output.py", command=execute_output)
+    execute_button.grid(row=1, column=3, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+    previous_messages = []
+
+    root.protocol("WM_DELETE_WINDOW", exit_program)
+
+    root.mainloop()
